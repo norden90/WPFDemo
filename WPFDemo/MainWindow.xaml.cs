b97140c17d17cf79c01cd83f4d7e1afc64a182a9
norden90 <nordfeldt.andreas@gmail.com>
2022-10-05 15:13:17 +0200

    asd

---
+++ WPFDemo/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button btn)
+            {
+                if (btn.Content != null)
+                    MinText.Text = btn.Content.ToString();
+                
+                
+            }
+            else
+            {
+
+            }
+            MinText.Text = Input.Text;
+        }
+
+        private void IncreaseFontSize(object sender, RoutedEventArgs e)
+        {
+            MinText.FontSize += 10;
+        }
+
+        private void LowerFontSize(object sender, RoutedEventArgs e)
+        {
+            MinText.FontSize -= 10;
+        }
+
+        private void FontComic(object sender, RoutedEventArgs e)
+        {
+            MinText.FontFamily = new FontFamily("Comic Sans MS");
+        }
+
+        private void FontCon(object sender, RoutedEventArgs e)
+        {
+            MinText.FontFamily = new FontFamily("Consolas");
+        }
+    }
+    
+}
